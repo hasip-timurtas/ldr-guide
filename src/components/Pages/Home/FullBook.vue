13593fc9024286dd4dc7228e28117fbdf4e0155b
root <root@host.payvolut.com>
2020-12-20 17:57:05 +0000

    full book with paypal published

--- src/components/Pages/Home/FullBook.vue
+++ src/components/Pages/Home/FullBook.vue
@@ -1,35 +1,42 @@
 <template>
   <div class="text-center">
-    <h2 class="mt-10">Lanunching on December, stay tuned on Instagram!</h2>
-    <VueCountdown :time="countDown">
-      <template slot-scope="props">
-        <h1 class="pa-5">
-          Time Remaining： {{ props.days }} days : {{ props.hours }} hours : {{ props.minutes }} minutes : {{ props.seconds }} seconds.
-        </h1>
-      </template>
-    </VueCountdown>
+    <h2 class="mt-10">Price of the full book is only 7.5 $</h2>
+    <div class="pa-10">
+      <PayPal
+        amount="7.50"
+        currency="USD"
+        :client="credentials"
+        env="production"
+      >
+      </PayPal>
+    </div>
+    <h3>
+      After you do the payment you will receive the book to your paypal email
+      address. If you want to receive to another mail address. Please write us
+      on Instagram:
+      <a href="https://www.instagram.com/ldrguide/">Here</a>
+    </h3>
     <v-img src="@/assets/book-chapters.png" max-width="300px" class="ma-auto" />
   </div>
 </template>
-
 <script>
-import VueCountdown from "@chenfengyuan/vue-countdown";
-
+import PayPal from "vue-paypal-checkout";
 export default {
-  components: { VueCountdown },
+  components: {
+    PayPal,
+  },
   data() {
     return {
-      countDown: 0
+      credentials: {
+        sandbox:
+          "AQsoDvd_zHW-cyXpNyPUa2dEtpeWOkbsPYb7bGZcXWu4bYk4c1mqQSgqH3WFE6gLwcPlz4E-XvHHxmY5",
+        production:
+          "ASWln5qx2YwovBa8WvBdM1prFz4TrxS14Gihz84riiTCZvoJTsVFOQD-ExwUu_7qVioLfa6TqsH5Q9V0",
+      },
     };
   },
-  created() {
-    const launchDateMilisecond = new Date("2020-12-21T23:59:57.000Z").getTime();
-    const currentDateMilisecond = new Date().getTime();
-    this.countDown = launchDateMilisecond - currentDateMilisecond;
-  }
 };
 </script>
-
 <style lang="scss">
 .zoid-outlet {
   width: 300px !important;
